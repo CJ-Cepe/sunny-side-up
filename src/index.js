f3750bc291cd7d16b9eba688b0b1774845d83904
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-12-14 22:40:17 +0800

    Build: set main containers and create css file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { getData } from "./dataRetriever"
+import { layoutMain } from "./layoutManager";
+
 
 (function () {
+    layoutMain()
     getData();
 })()
--- src/layoutManager.js
+++ src/layoutManager.js
@@ -1,5 +1,19 @@
 
 function layoutMain(){
+    const doc = createElements()
+    addClass(doc.mainCont)
+    //add class to all cont
+}
+
+function addClass(mainCont){
+    const containers = mainCont.querySelectorAll('div');
+    containers.forEach(container => {
+        container.classList.add('cont')
+    });
+}
+
+function createElements(){
+    const mainCont = document.createElement('main');
     const logoCont = document.createElement('div');
     const emptyCont = document.createElement('div');
     const currentCont = document.createElement('div');
@@ -9,6 +23,12 @@ function layoutMain(){
     const nextDay1 = document.createElement('div');
     const nextDay2 = document.createElement('div');
     const nextDay3 = document.createElement('div');
-    
+    const body = document.querySelector('body');
 
+    body.appendChild(mainCont)
+    mainCont.append(logoCont, emptyCont, currentCont, currDay, searchCont, windCont, nextDay1, nextDay2, nextDay3)
+
+    return {mainCont, logoCont, emptyCont, currentCont, currDay, searchCont, windCont, nextDay1, nextDay2, nextDay3, body}
 }
+
+export {layoutMain}
