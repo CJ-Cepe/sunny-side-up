e97d943dc06d0990dcac99f77a07277d7165429c
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-12-15 18:55:18 +0800

    Build: add rules to webpack common configuration

--- webpack.common.js
+++ webpack.common.js
@@ -21,10 +21,9 @@ const path = require('path');
    },
    module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader']}, 
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource' },
     ]
    }
  };
